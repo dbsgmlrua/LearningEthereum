8b340924969911665e39cd922e5123b5211cd016
윤희겸 <dbsgmlrua@naver.com>
2021-10-13 16:26:53 +0900

    first contract

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.0;
 
 contract SocialNetwork {
-    string name;
+    string public name;
     
+    constructor() public{
+        name = "Dapp University Social Network";
+    }
 }
